6290ff145de473b1565b8709b2358fabe73bf976
inzamamul <inzamamul.rifat@gmail.com>
2023-08-07 14:58:05 +0600

    init project

---
+++ src/main/java/com/inzamamul/experiment/CiCdIntegrationApplication.java
@@ -0,0 +1,21 @@
+package com.inzamamul.experiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CiCdIntegrationApplication {
+
+	@GetMapping()
+	public String get() {
+		return "inzamamul";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inzamamul/experiment/CiCdIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inzamamul.experiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
