b9e272a1bfe2b0e961ce70a701d5689397a16b46
inzamamul <inzamamul.rifat@gmail.com>
2023-08-07 16:25:08 +0600

    hot fixed

--- src/main/java/com/inzamamul/experiment/CiCdIntegrationApplication.java
+++ src/main/java/com/inzamamul/experiment/CiCdIntegrationApplication.java
@@ -6,18 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class CiCdIntegrationApplication {
-
-	@GetMapping()
-	public String get() {
-		return "inzamamul";
-	}
-
-	@GetMapping("hello")
-	public String hello() {
-		return "inzamamul";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CiCdIntegrationApplication.class, args);
---
+++ src/main/java/com/inzamamul/experiment/api/TestController.java
@@ -0,0 +1,19 @@
+package com.inzamamul.experiment.api;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping()
+    public String get() {
+        return "inzamamul";
+    }
+
+    @GetMapping("hello")
+    public String hello() {
+        return "inzamamul";
+    }
+}
